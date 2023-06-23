0c111a9bc3a516c60f8dd01bbf10161c0b36c94d
BilalMohammad1 <135028334+BilalMohammad1@users.noreply.github.com>
2023-06-23 11:19:55 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -3,37 +3,63 @@
 <head>
     <title>Fusion de branches Git</title>
     <link rel="stylesheet" type="text/css" href="style.css">
-    <meta charset="utd-8">
+    <link rel="stylesheet" type="text/css" href="style2.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
     <style>
         .center-form {
             text-align: center;
+            margin: 0 auto;
+            max-width: 600px;
+        }
+
+        .branch-selector {
+            display: flex;
+            align-items: center;
+            margin-bottom: 10px;
+        }
+
+        .branch-selector .delete-button {
+            margin-left: 10px;
+            color: red;
+            cursor: pointer;
         }
     </style>
+    <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
 </head>
 <body>
+    <?php
+    function getBranches() {
+        $repo_url = "https://webhook:Nzc3MDM2ODM5NzQzOjKx+GyGOlxvpdcLT0yBtfmtX7QO@bitbucket.fr.exalog.net/scm/bankx/bankx-sandbox.git";
+        $command = "git ls-remote --refs $repo_url | awk '{print $2}' | sed 's/refs\\/heads\\///' | sed 's/refs\\/tags\\///'";
+        $output = shell_exec($command);
+        $branches = explode("\n", trim($output));
+        return $branches;
+    }
+    ?>
+
     <div class="container">
         <div class="image">
             <img src="exalog.png" alt="Logo" class="logo">
         </div>
         <h1>Fusion de branches Git</h1>
-        <form action="merge.php" method="POST" target="blank" class="center-form">
-        
-            <label for="branch1">Branche 1:</label>
-            <select name="branch1" id="branch1" required>
-                <!-- Options pour les branches disponibles -->
-                <option value="ctchelidze_BANKX-74700_BANKX-74701_from_version/bankx-v16.15.0-max">ctchelidze_BANKX-74700_BANKX-74701_from_version/bankx-v16.15.0-max</option>
-                <option value="other_branch">Autre branche</option>
-                <!-- Ajoutez d'autres options pour les branches -->
-            </select><br><br>
-
-            <label for="branch2">Branche 2:</label>
-            <select name="branch2" id="branch2" required>
-                <!-- Options pour les branches disponibles -->
-                <option value="athevenet_BANKX-74845_BANKX-74846_from_version/bankx-v16.15.0-max">athevenet_BANKX-74845_BANKX-74846_from_version/bankx-v16.15.0-max</option>
-                <option value="other_branch">Autre branche</option>
-                <!-- Ajoutez d'autres options pour les branches -->
-            </select><br><br>
-            
+        <div class="add-button-container">
+            <button onclick="addBranchSelector()">+</button>
+        </div>
+        <form action="merge.php" method="POST" target="_blank" class="center-form">
+            <div class="branch-container">
+                <label for="branches[]">Branche 1:</label>
+                <select name="branches[]" id="branches[]" class="searchable-select" required>
+                    <?php
+                    $branches = getBranches();
+                    foreach ($branches as $branch) {
+                        echo "<option value=\"$branch\">$branch</option>";
+                    }
+                    ?>
+                    <option value="other_branch">Autre branche</option>
+                </select><br><br>
+            </div>
+            <div id="branch-selectors" class ="branch-selectors-container"></div>
 
             <label for="new_branch_name">Nouvelle branche:</label>
             <input type="text" name="new_branch_name" id="new_branch_name" placeholder="Insérer le nom de la nouvelle branche" style="width: 300px;" required><br><br>
@@ -41,12 +67,74 @@
             <input type="checkbox" name="delete_clone" id="delete_clone">
             <label for="delete_clone">Supprimer le clone du référentiel après la fusion</label><br><br>
 
-            <input type="submit" name="submit" class="button" value="Fusionner et obtenir les commits">
-
+            <input type="submit" name="submit" value="Fusionner les branches">
         </form>
     </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
+    <script>
+        var branchCounter = 1;
+
+        $(document).ready(function() {
+            $(".searchable-select").select2();
+
+            $("#branches\\[\\]").change(function() {
+                if ($(this).val() == "other_branch") {
+                    addBranchSelector();
+                    $(this).val("");
+                }
+            });
+        });
+
+        function addBranchSelector() {
+            var selector = document.createElement("div");
+            selector.classList.add("branch-selector");
+
+            var branchLabel = document.createElement("label");
+            branchLabel.textContent = "Branche " + (branchCounter + 1) + ":";
+
+            var select = document.createElement("select");
+            select.name = "branches[]";
+            select.classList.add("searchable-select","branch-select", "branch-label");
+
+            var branches = <?php echo json_encode($branches); ?>;
+            for (var i = 0; i < branches.length; i++) {
+                var option = document.createElement("option");
+                option.value = branches[i];
+                option.text = branches[i];
+                select.appendChild(option);
+            }
+
+            var deleteButton = document.createElement("i");
+            deleteButton.classList.add("fas", "fa-times", "delete-button");
+            deleteButton.onclick = function() {
+                deleteBranchSelector(selector);
+            };
+
+            selector.appendChild(branchLabel);
+            selector.appendChild(select);
+            selector.appendChild(deleteButton);
+            selector.appendChild(document.createElement("br"));
+            selector.classList.add("branch-selector");
+            document.getElementById("branch-selectors").appendChild(selector);
+
+            $(".searchable-select").select2();
+
+            branchCounter++;
+        }
+
+        function deleteBranchSelector(selector) {
+            selector.remove();
+        }
+    </script>
 </body>
 </html>
+
+
+
+
+
 
 
 
--- merge.php
+++ merge.php
@@ -1,14 +1,4 @@
 <?php
-var_dump($_POST['submit']);
-var_dump($_POST['new_branch_name']);
-var_dump($_POST['branch1']);
-var_dump($_POST['branch2']);
-
-/** 
- * Fonction récursive pour supprimer un répertoire et son contenu
- * @param string $dir Chemin vers le répertoire
- * @return bool True si la suppression est réussie, sinon False
- */
 function deleteDirectory($dir) {
     if (!file_exists($dir)) {
         return true;
@@ -32,33 +22,27 @@ function deleteDirectory($dir) {
 }
 
 if (isset($_POST['submit'])) {
-
-
-    $repository_path = "/apache/htdocs/exalogv428/bmohammad/FinalProject"; // Chemin du référentiel Git
+    $repository_path = "/apache/htdocs/exalogv428/bmohammad/TEST"; // Chemin du référentiel Git
     $user = "bmohammad";
     $repo_url = "https://webhook:Nzc3MDM2ODM5NzQzOjKx+GyGOlxvpdcLT0yBtfmtX7QO@bitbucket.fr.exalog.net/scm/bankx/bankx-sandbox.git";
     $bankx_sandbox_path = $repository_path . "/bankx-sandbox";
-    
-    if(is_dir($bankx_sandbox_path)){
-        echo" <br> <h1> Le répertoire existe déjà</h1>";
-    }else{
 
+    if (is_dir($bankx_sandbox_path)) {
+        echo "<br> <h1>Le répertoire existe déjà</h1>";
+    } else {
         $command_clone = "git clone $repo_url ";
         $output_clone = shell_exec($command_clone);
-        var_dump($output_clone);
-
     }
 
-    
     $repository_path .= "/bankx-sandbox";
-    if (!is_dir($repository_path)){
-        echo"<h2> Le répertoire cloné n'existe pas.</h2>";
+    if (!is_dir($repository_path)) {
+        echo"<h2>Le répertoire cloné n'existe pas.</h2>";
         exit;
-    }else{
-        echo"<h2> Le répertoire a bien été cloné.</h2>";
+    } else {
+        echo"<div class ='repertoire'><h2>Le répertoire a bien été cloné.</h2><br></div>";
     }
-    
-    if(isset($_POST['delete_clone'])){
+
+    if (isset($_POST['delete_clone'])) {
         if (is_dir($repository_path)) {
             // Supprimer le répertoire et son contenu récursivement
             $success = deleteDirectory($repository_path);
@@ -80,82 +64,63 @@ if (isset($_POST['submit'])) {
         chdir($repository_path);
 
         // Récupérer les valeurs du formulaire
-        $branch1 = $_POST['branch1'];
-        $branch2 = $_POST['branch2'];
+        $branches = $_POST['branches'];
         $new_branch_name = $_POST['new_branch_name'];
 
         // Vérifier si la branche de destination existe déjà
         $command_check_branch = "git rev-parse --quiet --verify $new_branch_name";
         $output_check_branch = shell_exec($command_check_branch);
-        var_dump($output_check_branch);
 
-        /*if (empty($output_check_branch)) {*/
 
-            $command_remote_branch = " git ls-remote --refs $repo_url | awk '{print $2}' | sed 's/refs\/heads\///' | sed 's/refs\/tags\///'";
-            $output_remote_branch = shell_exec($command_remote_branch);
-
-            $command_check_branch1 = "git checkout $branch1";
-            $output_check_branch1 = shell_exec($command_check_branch1);
-            var_dump($output_check_branch1);
-
-            $command_check_branch2 = "git checkout $branch2";
-            $output_check_branch2 = shell_exec($command_check_branch2);
-            var_dump($output_check_branch2);
-
-            $command_check_base = "git merge-base $branch1 $branch2";
-            $output_check_base = shell_exec($command_check_base);
-            if(!empty(trim($output_check_base))){
-                // Basculer vers la nouvelle branche-
-                $command_checkout_branch = "git checkout -b $new_branch_name";
-                $output_checkout_branch = shell_exec($command_checkout_branch);
-                var_dump($output_checkout_branch);
-
-                $command_merge = "git merge --no-ff --no-commit $branch1 $branch2";
-                $output_merge = shell_exec($command_merge);
-                var_dump($output_merge);
-            }else{
-                echo "Il n'y a pas d'ancêtre commun dans les branches que vous avez sélectionnés";
-            }
+            // Checkout de la nouvelle branche
+            $command_checkout_new_branch = "git checkout -b $new_branch_name";
+            $output_checkout_new_branch = shell_exec($command_checkout_new_branch);
             
-            // Effectuer le commit des modifications de fusion
-            $commit_message = 'Commit de fusion de '.$branch1.' avec '.$branch2;
+            if($output_checkout_new_branch === null){
+
+            
+                // Fusion de toutes les branches sélectionnées dans la nouvelle branche
+                foreach ($branches as $branch) {
+
+                    // Fusion de la branche
+                    $command_merge = "git merge --ff-only --no-commit $branch";
+                    $output_merge = shell_exec($command_merge);
+                }
+            }
+
+            $command_checkout_new_branch = "git checkout $new_branch_name";
+            $output_checkout_new_branch = shell_exec($command_checkout_new_branch);
+
+            // Commit des modifications de fusion
+            $commit_message = 'Commit de fusion de ' . implode(', ', $branches);
             $command_commit = "git commit -am '$commit_message'";
-            $command_commit .= " --quiet"; // Utiliser l'option --quiet pour supprimer la sortie non désirée
-            echo " commit : $command_commit";
-            
-            // Exécuter la commande avec la fonction exec
+            $command_commit .= " --quiet";
+
             exec($command_commit, $output_commit, $return_code);
-            var_dump($return_code);
-            echo " return : $return_code";
-            
+
             if ($return_code === 0) {
-                echo "Le commit a été effectué avec succès.";
+                // Pousser le commit vers le référentiel distant
+                $command_push = "git push origin $new_branch_name";
+                $output_push = shell_exec($command_push);
+
+                echo "<h2>Les branches sélectionnées ont été fusionnées avec succès dans la nouvelle branche '$new_branch_name'.</h2>";
             } else {
-                echo "Une erreur s'est produite lors de l'exécution du commit.";
+                echo "<h2>Une erreur s'est produite lors de la fusion des branches.</h2>";
             }
-
-            // Afficher les résultats
-            echo "<h2>Nouvelle branche '$new_branch_name' créée et activée.</h2>";
-            echo "<h2>Commits :</h2>";
-            echo "<pre>$output_log</pre>";
-        /*} else {
-            echo "<h2>La branche '$new_branch_name' existe déjà.</h2>";
-        }*/
-
-        $command_push = "git push $repo_url --set-upstream $new_branch_name";
-        var_dump($command_push);
-        $output_push = shell_exec($command_push);
-        var_dump($output_push);
-
-        // Vérifier si le push a réussi
-        if (strpos($output_push, 'Everything up-to-date') !== false) {
-            echo "<h2>Push réussi vers l'origine.</h2>";
-        } else {
-            echo "<h2>Erreur lors du push vers l'origine.</h2>";
-
-        }
     } else {
-        echo "<h2>Chemin du référentiel invalide.</h2>";
+        echo "<div class ='message'><h2>Le répertoire spécifié n'est pas un référentiel Git valide.</h2></div>";
     }
 }
 ?>
+
+<style>
+    <?php 
+    include 'style2.css';
+    include 'style.css';
+    ?>
+</style>
+
+
+
+
+
